8b8b0a6b0fa12f568c0ef7cd274cf8d94e33f359
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 01:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const returnLastTwoDrivers = (function (drivers) {return drivers.slice(-2)})
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 // + `createFareMultiplier()` — This is a higher-order function that takes in one argument, an integer, and returns a function that will multiply a fare for a ride accordingly. If `createFareMultiplier()` receives an argument of `4`, it will return a function that takes in a fare as an argument and quadruples the fare.
 const createFareMultiplier = function(multiplier) {
-  return function()
+  return function(fare) {
+    return fare * multiplier
+  }
 }
 // + `fareDoubler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` function accepts a fare as its lone argument and doubles it.
 // + `fareTripler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareTripler()` function accepts a fare as its lone argument and triples it.
