6635a172a402560ec5a3f13d96299f558f122be5
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-27 01:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const createFareMultiplier = function(multiplier) {
 // + `fareDoubler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareDoubler()` function accepts a fare as its lone argument and doubles it.
 const fareDoubler = createFareMultiplier(2)
 // + `fareTripler()` — Declare a variable with `const` and assign a function returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in such a way that the new `fareTripler()` function accepts a fare as its lone argument and triples it.
-const fareDoubler = createFareMultiplier(3)
+const fareTripler = createFareMultiplier(3)
 // + `fetchSpecifiedDrivers()` — This function takes two arguments, an array of `drivers` and either the `returnFirstTwoDrivers()` or `returnLastTwoDrivers()` function. Based on these two arguments, `fetchSpecifiedDrivers()` will return either the first two drivers or the last two drivers.
-// const fetchSpecifiedDrivers = (function (drivers, func) {
-//   return func(drivers)
-// }
+const fetchSpecifiedDrivers = (function (drivers, func) {
+  return func(drivers)
+}
